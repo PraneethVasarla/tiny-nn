2e8f0bf855d2e367db793cde12ffe6297fcfb20e
Praneeth Vasarla <praneethvaasarla@gmail.com>
2023-06-14 17:01:23 +0530

    changed init files for models n loss_functions

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='tiny-nn',
+    name='tinynn-py',
     version='0.1.0',
     packages=find_packages(),
     author='Praneeth Vasarla',
