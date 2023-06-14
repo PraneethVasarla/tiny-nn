1299ef19fb007365a2cee526078086c1fb78595b
Praneeth Vasarla <praneethvaasarla@gmail.com>
2023-06-14 16:32:10 +0530

    changed README.md and added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tinynn',
+    version='0.1.0',
+    packages=find_packages(),
+    author='Praneeth Vasarla',
+    description='A tiny neural network library.',
+    install_requires=[
+        "numpy"
+    ],
+)
